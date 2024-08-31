6a680c031f5928cc0198413f98748a6f1c9e09fb
Ayrton Morrison <ayrtonmorrison15@gmail.com>
2024-08-31 19:27:51 -0300

    Se anhade el comando -h, el cual muestra los comandos que se trabajaran en el proyecto

--- main.c
+++ main.c
@@ -20,6 +20,7 @@
  * @param argv argumentos ingresados en el terminal. argv[1] es el primero que se ingresa, argv[2] el segundo, y así
  * 
  *  
+suma.o: suma.c suma.h
  */
 
 int main(int argc, char** argv){
@@ -37,7 +38,9 @@ int main(int argc, char** argv){
     while((option = getopt(argc, argv, "h"))!=-1){
        switch (option) {
             case 'h':
-                printf("funciona");
+                printf("Estos son comandos comunes de uGit usados en distintas situaciones:\n\n");
+						printf("Para comenzar un area de trabajo\n   init       Crear un repositorio vacio\n\nTrabajar y realizar cambios actuales\n   add        Agregar contenido de archivos\n   rm         Borrar archivos del arbol de trabajo\n\nCrecer, marca y ajustar el historial comun\n   branch     Listar, crear o borrar ramas\n   commit     Grabar los cambios del repositorio\n   switch     Cambiar el nodo/branch\n\nPara colaborar\n   pull       Realiza una descarga de objetos y referencias de otro repositorio y lo integra al actual o al de la rama actual\n   push       Actualizar las referencias remotars con sus objetos asociados\n");
+
                 break;
             case '?':
                 printf("se puso otra cosa");
