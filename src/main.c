c73c2156ac22463842115927a4ce266e075ff568
Ayrton Morrison <ayrtonmorrison15@gmail.com>
2024-09-03 00:56:51 -0300

    Se corrigio la indentacion, documentacion, orden y logica del codigo

--- src/main.c
+++ src/main.c
@@ -25,47 +25,55 @@
 
 int main(int argc, char** argv){
 
+	// EN EL CASO DE QUE SE INGRESE UN COMANDO
+    if(argc>1){ 
 
-    if(argc>1){ //si se colocó algún argumento
-
-      if(strcmp(argv[1],"help")==0){ // comando para mostrar ayuda
-        printf("Comandos comunes de uGit:\n\n");
-				printf("Para inicializar un area de trabajo\n   init       Crear un repositorio vacio\n\nTrabajar y realizar cambios actuales\n   add        Agregar contenido de archivos\n   rm         Borrar archivos del arbol de trabajo\n\nModificar el historial comun\n   branch     Listar, crear o borrar ramas\n   commit     Grabar los cambios del repositorio\n   switch     Cambiar el nodo/branch\n\nPara colaborar\n   pull       Realiza una descarga de objetos y referencias de otro repositorio y lo integra al actual o al de la rama actual\n   push       Actualizar las referencias remotas con sus objetos asociados\n");
-      } else
+		// COMANDO PARA MOSTRAR LOS COMANDOS DISPONIBLES
+		if(strcmp(argv[1],"help")==0){ 
+			printf("Comandos comunes de uGit:\n\n");
+			printf("Para inicializar un area de trabajo\n   init       Crear un repositorio vacio\n\nTrabajar y realizar cambios actuales\n   add        Agregar contenido de archivos\n   rm         Borrar archivos del arbol de trabajo\n\nModificar el historial comun\n   branch     Listar, crear o borrar ramas\n   commit     Grabar los cambios del repositorio\n   switch     Cambiar el nodo/branch\n\nPara colaborar\n   pull       Realiza una descarga de objetos y referencias de otro repositorio y lo integra al actual o al de la rama actual\n   push       Actualizar las referencias remotas con sus objetos asociados\n\n");
+      	}	 	
+		
 
 
       /*COMANDO PARA INICIALIZAR REPOSITORIO*/
-      if(strcmp(argv[1],"init")==0){
+		else if(strcmp(argv[1],"init")==0){
+			
+			// EN EL CASO DE ESTAR INICIALIZADO AVISAR QUE YA LO ESTA
+			if(is_initialized()){
+				printf("uGit ya se encuentra inicializado en el directorio\n");
+			}
 
-          if(isinitialized()){// Si ya está inicializado,  mostrar mensaje de que ya lo está
-            printf("uGit ya se encuentra inicializado en el directorio");
-          }
-          else{ // Si no está inicializado, inicializarlo
-            system("touch .ugit");
-            if(isinitialized())
-              printf("Repositorio inicializado exitosamente");
-            else
-              printf("ERROR: No se pudo inicializar uGit");
-          }
-      }
+			// INICIALIZAR EL REPOSITORIO
+          	else{ 
 
+				system("touch .ugit");
 
+				// ALERTA INICIACION EXITOSA
+				if(is_initialized()){
+					printf("Repositorio inicializado exitosamente\n");
+				}
 
+				// DEPURACION
+				else{
+					printf("ERROR: No se pudo inicializar uGit\n");
+				}
+			}
 
+        }
+      
 
-      else{ // si no se  colocó un comando válido
-        printf("Uso: 'ugit [comando]'\n'ugit help' para ver la lista de comandos disponibles");
-      }
-
-
-
-    
-
+		// EN EL CASO DE COLOCAR UN COMANDO NO VALIDO
+		else{ 
+			printf("Comando invalido\n'ugit help' para ver la lista de comandos disponibles\n");
+		}
 
 
     }
-    else { //si ugit es ejecutado sin ningún argumento
-      printf("Uso: 'ugit [comando]'\n'ugit help' para ver la lista de comandos disponibles");
+	
+	//EN EL CASO DE QUE UGIT SEA EJECUTADO SIN NINGUN ARGUMENTO
+    else { 
+		printf("No ingreso nignun comando'\n'ugit help' para ver la lista de comandos disponibles\n");		
     }
 
   return 0;
