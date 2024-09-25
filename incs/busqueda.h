33dcdc1ba72348da74d146bf08b1c9cb71c9622c
ivnmansi <ivan.mansillacor@gmail.com>
2024-09-24 23:11:51 -0300

    debug

--- incs/busqueda.h
+++ incs/busqueda.h
@@ -51,11 +51,6 @@ unsigned int Dinamic_jenkins ( unsigned char * key , size_t lenght );
 void init_table(HashTable *hashtable);
 void insert_hash(HashTable * hashtable, char* key, int value, int * columns);
 void insert_hashfile(HashTable * hashtable, const char * filename, int value, int * columns);
-int search_table(HashTable * hashtable, char * key);
-int search_tablefile(HashTable * hashtable, const char * filename);
-void delete_element(HashTable * hashtable, char * key);
-void delete_elementfile(HashTable * hashtable, const char * filename);
-void print_table(HashTable* hashtable);
 void print_tablefile(HashTable* hashtable, const char *filename);
 void save_table(HashTable* hashtable, const char * filename);
 
